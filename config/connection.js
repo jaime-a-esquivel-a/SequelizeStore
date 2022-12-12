bab0d6b7de972664f5c5430334113c37a75a1665
Jaime Alberto Esquivel Acosta <jaimeesquivel@Jaimes-MacBook-Pro.local>
2022-12-12 15:11:12 -0600

    Config folder update

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
+const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const Sequelize = require('sequelize');
-
+/*
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
@@ -11,5 +11,21 @@ const sequelize = process.env.JAWSDB_URL
         decimalNumbers: true,
       },
     });
+*/
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306,
+    dialectOptions: {
+      decimalNumbers: true,
+    },
+  }
+);
+
 
 module.exports = sequelize;
